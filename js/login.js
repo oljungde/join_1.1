ef784f56eafddb396964df6d81545eadf8ade705
oljungde <oljung@posteo.de>
2023-03-19 17:17:10 +0100

    bugfix login, user could login again

--- js/login.js
+++ js/login.js
@@ -72,14 +72,17 @@ function isRememberMe() {
  */
 function checkUserData(userEmail, userPassword) {
     let indexOfEmail = users.findIndex(user => user.email == userEmail);
-    if (indexOfEmail != -1) {
-        let loginPassword = users[indexOfEmail].password;
-    }
-    if (indexOfEmail == -1 || loginPassword != userPassword) {
+    if (indexOfEmail == -1) {
         let dataCheck = document.getElementById('data_check');
         dataCheck.classList.remove('display-none');
     } else {
-        userLogIn(indexOfEmail);
+        let loginPassword = users[indexOfEmail].password;
+        if (userPassword != loginPassword) {
+            let dataCheck = document.getElementById('data_check');
+            dataCheck.classList.remove('display-none');
+        } else {
+            userLogIn(indexOfEmail);
+        }
     }
 }
 
